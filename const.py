a410f7ddb87a614928dd7619870e550291150a50
Xiangdong Liang <59079800+522blackcat@users.noreply.github.com>
2025-08-19 10:42:49 +0800

    Update const.py

--- const.py
+++ const.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import rule_engine
 
 
@@ -15,8 +17,21 @@ DB_TYPE_TRANSFORM_RULE = {
     "float": RULE_TYPE_FLOAT,
     "boolean": RULE_TYPE_BOOLEAN,
     "date": RULE_TYPE_DATE,
-    "datetime": RULE_TYPE_DATETIME
+    "datetime": RULE_TYPE_DATETIME,
+    "text": RULE_TYPE_VARCHAR,
 }
+
+DB_TYPE_TRANSFORM_DISPLAY = {
+    "varchar": "String",
+    "text": "String",
+    "datetime": "Time",
+    "date": "Date",
+    "int": "Int",
+    "float": "Float",
+}
+
+SPECIAL_DB_TYPE = ('int', 'float', 'date', 'datetime', 'varchar', 'text')
+
 
 ROW_CHECK_CATEGORY = "row"
 COL_CHECK_CATEGORY = "col"
@@ -38,3 +53,40 @@ def get_rule_text_by_require(field: str, required: str) -> str:
     if required == FALSE_REQUIRE:
         return ""
     return ''
+
+
+def get_rule_text_by_type(field: str, _type: str) -> str:
+    if _type in ['varchar', 'text']:
+        return f"{field}=~'.*'"
+    if _type == "int":
+        return f"{field}//1=={field}"
+    if _type == "float":
+        return f"({field}>=0 or {field}<0)"
+    if _type == "date":
+        return f"{field}>=d'1970-01-01 00:00:00'"
+    if _type == "datetime":
+        return f"{field}>=d'1970-01-01 00:00:00'"
+    return ""
+
+
+# 检验规则的类型
+class CheckerType(Enum):
+    CONTENT_INVALID = "Invalid Value"
+    DATA_TYPE = "Data Type"
+    REQUIRED = "Required"
+    DUPLICATE = "Duplicate Data"
+    UNKNOWN = "Unknown Error"
+
+
+class FileCheckMessageError:
+    MESSAGE = {
+        CheckerType.CONTENT_INVALID.value: "Invalid content value:",
+        CheckerType.DATA_TYPE.value: "Field type mismatch:",
+        CheckerType.REQUIRED.value: "Missing required fields:",
+        CheckerType.DUPLICATE.value: "Duplicate data found:",
+        CheckerType.UNKNOWN.value: "Unknown Error:",
+    }
+
+    @classmethod
+    def get_message_by_check_type(cls, check_type):
+        return cls.MESSAGE.get(check_type, CheckerType.UNKNOWN.value)
